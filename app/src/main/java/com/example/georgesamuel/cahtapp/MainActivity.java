6fc0e82724e628d037ccfad7c3dd1a38647856de
George <georgesamuel96>
2019-10-18 20:53:23 +0200

    Firebase commit

--- app/src/main/java/com/example/georgesamuel/cahtapp/MainActivity.java
+++ app/src/main/java/com/example/georgesamuel/cahtapp/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.georgesamuel.cahtapp;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.btnSend)
+    Button btnSend;
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mDatabase = FirebaseDatabase.getInstance();
+        mRef = mDatabase.getReference("copyright");
+        mRef.child("test").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                Log.d(TAG, "onDataChange: " + user.getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, "onCancelled: " + databaseError.getMessage());
+            }
+        });
+    }
+
+    @OnClick(R.id.btnSend)
+    public void onSendClicked() {
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", "George");
+        user.put("email", "george@samuel.com");
+        String userId = mRef.push().getKey();
+        mRef.child("test").setValue(user)
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_LONG).show();
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: " + e.getMessage());
+            }
+        });
+
     }
 }
