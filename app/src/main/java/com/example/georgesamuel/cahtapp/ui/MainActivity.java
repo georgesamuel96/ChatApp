19bdcf46577123b6dbfe2546b040abdefcf66813
George Samuel <gsamuel@istecheg.com>
2019-11-04 16:45:06 +0200

    deleted files commit

--- app/src/main/java/com/example/georgesamuel/cahtapp/ui/MainActivity.java
+++ app/src/main/java/com/example/georgesamuel/cahtapp/ui/MainActivity.java
@@ -30,67 +30,10 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-    @BindView(R.id.btnSend)
-    Button btnSend;
-    private FirebaseDatabase mDatabase;
-    private DatabaseReference mRef;
-    private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        mAuth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance();
-        mRef = mDatabase.getReference("copyright");
-        mRef.child("test").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                User user = dataSnapshot.getValue(User.class);
-                Log.d(TAG, "onDataChange: " + user.getName());
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.d(TAG, "onCancelled: " + databaseError.getMessage());
-            }
-        });
     }
 
-    @OnClick(R.id.btnSend)
-    public void onSendClicked() {
-        Map<String, Object> user = new HashMap<>();
-        user.put("name", "George");
-        user.put("email", "george@samuel.com");
-        String userId = mRef.push().getKey();
-        mRef.child("test").setValue(user)
-                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_LONG).show();
-                    }
-                }).addOnFailureListener(this, new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d(TAG, "onFailure: " + e.getMessage());
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.signout){
-            mAuth.signOut();
-            finish();
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/example/georgesamuel/cahtapp/ui/SplashActivity.java
+++ app/src/main/java/com/example/georgesamuel/cahtapp/ui/SplashActivity.java
@@ -29,94 +29,9 @@ import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final String TAG = SplashActivity.class.getSimpleName();
-    private GoogleSignInClient mGoogleSignInClient;
-    private static final int RC_SIGN_IN = 1;
-    private FirebaseAuth mAuth;
-    private boolean isSignedIn = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        mAuth = FirebaseAuth.getInstance();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            gotoMain();
-        }
-        else {
-            signIn();
-        }
-    }
-
-    private void gotoMain() {
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        finish();
-    }
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account);
-            } catch (ApiException e) {
-                Log.d(TAG, "Google sign in failed", e);
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            boolean isNew = task.getResult().getAdditionalUserInfo().isNewUser();
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            if(isSignedIn == true)
-                                return;
-                            isSignedIn = true;
-                            if(isNew){
-                                updateUINewAccount(user);
-                            }
-                            else{
-                                updateUI(user);
-                            }
-
-                        } else {
-                            Log.w(TAG, "signInWithCredential:failure ", task.getException());
-                            Toast.makeText(SplashActivity.this, "Authentication Failed.", Toast.LENGTH_LONG).show();
-                            updateUI(null);
-                        }
-                    }
-                });
-    }
-
-    private void updateUI(FirebaseUser user) {
-        gotoMain();
-    }
-
-    private void updateUINewAccount(FirebaseUser user) {
-        gotoMain();
     }
 }
